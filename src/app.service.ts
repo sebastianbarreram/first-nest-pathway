6c9cc3cf951bfd2813e331034b386bc26fbdd08c
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2022-11-03 15:40:42 -0500

    Update output message from endpoint localhost:3000

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Este es mi primera aplicación en Nest';
   }
 }
